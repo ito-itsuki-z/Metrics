077f8a0109655fe90e7cf05ac1bc7b97b6ce53e4
ito-itsuki-z <itsuki.ito@zigexn.co.jp>
2024-10-28 15:09:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ml_metrics',
     version='0.1.4',
-    packages=['ml_metrics'],
+    packages=find_packages(),
     install_requires=[],
     description='Machine Learning Metrics',
     url='https://github.com/ito-itsuki-z/Metrics',
