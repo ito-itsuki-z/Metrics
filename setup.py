62820e001c413148bd3fab15220274483b3da5f9
ito-itsuki-z <itsuki.ito@zigexn.co.jp>
2024-10-28 15:05:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ml_metrics',
+    version='0.1.4',
+    packages=['ml_metrics'],
+    install_requires=[],
+    description='Machine Learning Metrics',
+    url='https://github.com/ito-itsuki-z/Metrics',
+    author='Ben Hamner',
+    author_email='author@example.com',
+)
